c9669d51148e16dc6eb187731002ced0771b5ea0
Rohit Salitra <rohit.salitra07@gmail.com>
2023-12-24 21:32:12 +0530

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'admin/dashboard#index'
+  root to:  'home#index'
+  # root to: 'admin/dashboard#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
