ab8e64dbbd08a6893e2b5b90efae6d715ffd4c9b
Rohit Salitra <rohit.salitra07@gmail.com>
2024-01-03 14:01:12 +0530

    added creds for mailer in prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'employee-ease.onrender.com', port: 443 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com', 
+    user_name: 'rohit.salitra07@gmail.com',
+    password: 'lhduuopchmvrwydh',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
 end
